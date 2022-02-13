c21666be87111024e80dc3762620ded4e0857236
Sergey_Elekin <elekinsergey@gmail.com>
2022-02-13 23:24:53 +0400

    Создание 1 кейса

---
+++ src/main/java/page/dateGooglePage.java
@@ -0,0 +1,101 @@
+package page;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+import java.util.List;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+public class dateGooglePage {
+
+    @FindBy(css = "div [jsname=\"VkJw6\"] span")
+    public List<WebElement> checkInput;
+
+    @FindBy(css = "div [jsname=\"zLiRgc\"] span")
+    public List<WebElement> checkResult;
+
+    @FindBy(css = "input.gLFyf.gsfi")
+    private WebElement searchInput;
+
+    @FindBy(css = "div[aria-label=\"открывающая скобка\"]")
+    public WebElement openBracket;
+
+    @FindBy(css = "div[aria-label=\"закрывающая скобка\"]")
+    public WebElement closingBracket;
+
+    @FindBy(css = "div[aria-label=\"сложение\"]")
+    public WebElement plus;
+
+    @FindBy(css = "div[aria-label=\"вычитание\"]")
+    public WebElement minus;
+
+    @FindBy(css = "div[aria-label=\"умножение\"]")
+    public WebElement multiply;
+
+    @FindBy(css = "div[aria-label=\"деление\"]")
+    public WebElement division;
+
+    @FindBy(css = "div[aria-label=\"равно\"]")
+    public WebElement equally;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"bkEvMb\"]")
+    public WebElement zero;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"N10B9\"]")
+    public WebElement one;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"lVjWed\"]")
+    public WebElement two;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"KN1kY\"]")
+    public WebElement three;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"xAP7E\"]")
+    public WebElement four;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"Ax5wH\"]")
+    public WebElement five;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"abcgof\"]")
+    public WebElement six;
+
+    @FindBy(css = "div[aria-label=\"синус\"]")
+    public WebElement sin;
+
+
+    public dateGooglePage(WebDriver driver) {
+        initElements(driver, this);
+    }
+
+    public void search(String text) {
+        searchInput.sendKeys(text, Keys.ENTER);
+    }
+
+}
+//                driver.findElement(By.cssSelector("div[aria-label=\"открывающая скобка\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"синус\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"деление\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"умножение\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"Вычитание\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"сложение\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"равно\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"закрывающая скобка\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"открывающая скобка\"]")).click();
+//                //0
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"bkEvMb\"]")).click();
+//                //1
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"N10B9\"]")).click();
+//                //2
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"lVjWed\"]")).click();
+//                //3
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"KN1kY\"]")).click();
+//                //4
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"xAP7E\"]")).click();
+//                //5
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"Ax5wH\"]")).click();
+//                //6
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"abcgof\"]")).click();
---
+++ src/test/java/tests/googleCalcTests.java
@@ -0,0 +1,83 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import page.dateGooglePage;
+import page.dateGooglePage;
+
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class googleCalcTests {
+
+    private static WebDriver driver;
+    private static dateGooglePage dateGooglePage;
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        dateGooglePage = new dateGooglePage(driver);
+
+    }
+
+    @BeforeEach
+    public void setup() {
+
+        driver.get("https://www.google.ru/");
+    }
+
+    @Test
+    @DisplayName("Кейс 1. Проверка расчета формулы")
+    public void test1() {
+        dateGooglePage.search("Калькулятор");
+        //(
+        dateGooglePage.openBracket.click();
+        //1
+        dateGooglePage.one.click();
+        //+
+        dateGooglePage.plus.click();
+        //2
+        dateGooglePage.two.click();
+        //)
+        dateGooglePage.closingBracket.click();
+        //*
+        dateGooglePage.multiply.click();
+        //3
+        dateGooglePage.three.click();
+        //-
+        dateGooglePage.minus.click();
+        //4
+        dateGooglePage.four.click();
+        //0
+        dateGooglePage.zero.click();
+        //:
+        dateGooglePage.division.click();
+        //5
+        dateGooglePage.five.click();
+        //=
+        dateGooglePage.equally.click();
+
+        assertAll(
+                () -> assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", driver.findElement(By.cssSelector("div [jsname=\"VkJw6\"] span")).getText()),
+                () -> assertEquals("1", driver.findElement(By.cssSelector("div [jsname=\"zLiRgc\"] span")).getText())
+        );
+
+
+    }
+
+
+    @AfterAll
+    public static void teardown() {
+        driver.quit();
+    }
+
+}
