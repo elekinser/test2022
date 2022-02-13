c17eb1dc27439fc68a4df563e1e33802425e73a2
Sergey_Elekin <elekinsergey@gmail.com>
2022-02-13 23:26:32 +0400

    Создание 2 кейса

--- src/test/java/tests/googleCalcTests.java
+++ src/test/java/tests/googleCalcTests.java
@@ -73,6 +73,25 @@ public class googleCalcTests {
 
 
     }
+    @Test
+    @DisplayName("Кейс 2. Проверка деления на ноль")
+    public void test2() {
+        dateGooglePage.search("Калькулятор");
+        //6
+        dateGooglePage.six.click();
+        //:
+        dateGooglePage.division.click();
+        //0
+        dateGooglePage.zero.click();
+        //=
+        dateGooglePage.equally.click();
+        assertAll(
+                () -> assertEquals("6 ÷ 0 =", driver.findElement(By.cssSelector("div [jsname=\"VkJw6\"] span")).getText()),
+                () -> assertEquals("Infinity", driver.findElement(By.cssSelector("div [jsname=\"zLiRgc\"] span")).getText())
+        );
+
+
+    }
 
 
     @AfterAll
